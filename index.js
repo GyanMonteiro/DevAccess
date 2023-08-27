5d4987c968f7b2f53c4c8c1d7218a8a4e71000a1
GyanMonteiro <gyan229@hotmail.com>
2023-08-27 19:54:53 -0300

    Limpando
    Excluindo arquivos inúteis

--- index.js
+++
@@ -1,6 +0,0 @@
-const sideBar = document.getElementById("sidebar");
-const bttn = document.getElementById("bttn");
-
-bttn.addEventListener("click", () => {
-    sideBar.classList.toggle("close");
-})
